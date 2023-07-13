0a85a70b44135e53630a0132b278eacba2c3584d
Andropov Ajuatah Ajebua <ajuatahcodingarena@gmail.com>
2023-07-13 07:17:03 +0100

    Add project files.

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class StringExtensions
+    {
+        public static string Reverse(this string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "Andropov Ajebua";
+            string reversed = original.Reverse();
+            Console.WriteLine(reversed); // Output: "!aubejA vopordnA"
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
